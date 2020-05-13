5b971c12eb274bba0a3d87752c90e3679888eebe
“vishnu <vishnu@bflgroup.ae>
2020-05-13 21:44:07 +0400

    Updates

---
+++ precache-manifest.5c5f3028fffc40f95488769ac5e4d003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed11b3156a26572d2e6aa7a6645edda8",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "fbe65a93a77c0b656e31",
+    "url": "/sample/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/sample/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe65a93a77c0b656e31",
+    "url": "/sample/static/js/main.34cc7ca2.chunk.js"
+  },
+  {
+    "revision": "66763cce2864cffd1a3f",
+    "url": "/sample/static/js/runtime-main.7f06dffd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
